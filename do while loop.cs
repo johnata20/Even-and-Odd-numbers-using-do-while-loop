9b01908972ebeaee2608d29b62d9c7c114eb57a6
John Rodolfo <114643834+johnata20@users.noreply.github.com>
2022-10-02 02:25:06 -0600

    Add files via upload

---
+++ do while loop.cs
@@ -0,0 +1,35 @@
+﻿int inp, x;
+
+Console.Write("Enter an integer : ");
+inp = Convert.ToInt32(Console.ReadLine());
+
+if (inp > 0 && inp % 2 == 0)
+{
+    x = 2;
+    Console.Write("Even numbers from 1 to {0} : ", inp);
+    do
+    {
+        Console.Write("{0} ", x);
+        x += 2;
+    } while (x <= inp);
+}
+
+if (inp > 0 && inp % 2 != 0)
+{
+    x = 1;
+    Console.Write("Odd numbers from 1 to {0} : ", inp);
+    do
+    {
+        Console.Write("{0} ", x);
+        x += 2;
+    } while (x <= inp);
+}
+
+if (inp == 0)
+    Console.WriteLine("The input is zero.");
+
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
+
